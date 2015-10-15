9ebfa8fb0b0001cea72725355d317ee61d49a20d
Iridesent <blazingikari@yahoo.com>
2015-10-15 11:45:24 -0600

    Trying to add the Tests again

---
+++ Timesheet/TimesheetTests1/TimesheetGeneratorTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Timesheet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Timesheet.Tests
+{
+    [TestClass()]
+    public class TimesheetGeneratorTests
+    {
+        [TestMethod()]
+        public void CalculateOvertimeTestNone()
+        {
+            int hoursWorked = 35;
+            int expected = 0;
+            TimesheetGenerator tg = new TimesheetGenerator();
+           int returnedValue = tg.CalculateOvertime(hoursWorked);
+            Assert.AreEqual(returnedValue, expected);
+        }
+        [TestMethod()]
+        public void CalculateOvertimeTestWith()
+        {
+            int hoursWorked = 90;
+            int expected = 10;
+            TimesheetGenerator tg = new TimesheetGenerator();
+            int returnedValue = tg.CalculateOvertime(hoursWorked);
+            Assert.AreEqual(returnedValue, expected);
+        }
+    }
+}
