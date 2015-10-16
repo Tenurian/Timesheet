20e24416e5b535dc45837e7c49b41510c2ef884d
Joseph Wunz <jodaman19@gmail.com>
2015-10-16 11:33:22 -0600

    Committing everything to make Git happy.

---
+++ Timesheet/TimesheetTests1/TimesheetGeneratorTests.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Timesheet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Timesheet.Tests
+{
+    [TestClass()]
+    public class TimesheetGeneratorTests
+    {
+        [TestMethod()]
+        public void CalculateOvertimeTestNone()
+        {
+            int hoursWorked = 35;
+            int expected = 0;
+            TimesheetGenerator tg = new TimesheetGenerator();
+           int returnedValue = tg.CalculateOvertime(hoursWorked);
+            Assert.AreEqual(returnedValue, expected);
+        }
+        [TestMethod()]
+        public void CalculateOvertimeTestWith()
+        {
+            int hoursWorked = 90;
+            int expected = 10;
+            TimesheetGenerator tg = new TimesheetGenerator();
+            int returnedValue = tg.CalculateOvertime(hoursWorked);
+            Assert.AreEqual(returnedValue, expected);
+        }
+        [TestMethod()]
+        public void TimesheetGeneratorTest()
+        {
+            //aaa (Arrange Act Assert)
+            //Arrange
+            TimesheetGenerator tg1 = new TimesheetGenerator();      //DEFAULT 2 weeks, 40 Hours max
+            TimesheetGenerator tg2 = new TimesheetGenerator(1);     //1 week, default max
+            TimesheetGenerator tg3 = new TimesheetGenerator(1, 36);  //1 week, 36 hours max
+            //Act
+            //Assert
+            Assert.Fail();
+        }
+    }
+}
