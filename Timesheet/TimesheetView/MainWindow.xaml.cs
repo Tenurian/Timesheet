d2cb54a41361687b07f5e18d50189c4da20206a4
Thomas Feuerborn <Thomas Feuerborn>
2015-12-04 11:52:36 -0700

    added binding to day 1
    Signed-off-by: Thomas Feuerborn <Thomas Feuerborn>

--- Timesheet/TimesheetView/MainWindow.xaml.cs
+++ Timesheet/TimesheetView/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -28,7 +29,46 @@ namespace TimesheetView
         private void HD1_TextChanged(object sender, TextCompositionEventArgs e)
         {
             if (e.Text.Any(c => !char.IsDigit(c))) e.Handled = true;
-            
+
+        }
+
+        private void D1_Checked(object sender, RoutedEventArgs e)
+        {
+            HD1.Text = "8";
+        }
+    }
+
+    public class ValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            int hold;
+            Boolean test = Int32.TryParse(value.ToString(), out hold);
+
+            if (test)
+            {
+                if (hold > 24)
+                {
+                    return "Input too high";
+                }
+                else if (hold < 0)
+                {
+                    return "Input too low";
+                }
+                else
+                {
+                    return hold.ToString();
+                }
+            }
+            else
+            {
+                return "N/A";
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
         }
     }
 }
